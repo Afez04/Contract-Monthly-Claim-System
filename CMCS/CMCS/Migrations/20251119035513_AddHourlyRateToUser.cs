03f6d4ff2d5fdda480e81274d8026d40e5ab961b
Afeziwe Thandani <ST10448454@rcconnect.edu.za>
2025-11-22 01:21:01 +0200

    Added migration folder

---
+++ CMCS/CMCS/Migrations/20251119035513_AddHourlyRateToUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMCS.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHourlyRateToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "HourlyRate",
+                table: "Users",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HourlyRate",
+                table: "Users");
+        }
+    }
+}
